c54a995640c63ba4679129c5a1fd5cec9a2858e6
nannullna <nannullna00@gmail.com>
2022-02-03 15:44:05 +0900

    wandb sweep integration

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 from utils import get_dataset, get_net, get_strategy
 from pprint import pprint
+import wandb
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--seed', type=int, default=1, help="random seed")
@@ -24,26 +25,28 @@ parser.add_argument('--strategy_name', type=str, default="RandomSampling",
                              "AdversarialBIM", 
                              "AdversarialDeepFool"], help="query strategy")
 args = parser.parse_args()
-pprint(vars(args))
+wandb.init(config=args, project="dal", name=args.strategy_name)
+config = wandb.config
+pprint(vars(config))
 print()
 
 # fix random seed
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
+np.random.seed(config.seed)
+torch.manual_seed(config.seed)
 torch.backends.cudnn.enabled = False
 
 # device
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
-dataset = get_dataset(args.dataset_name)                   # load dataset
-net = get_net(args.dataset_name, device)                   # load network
-strategy = get_strategy(args.strategy_name)(dataset, net)  # load strategy
+dataset = get_dataset(config.dataset_name)                   # load dataset
+net = get_net(config.dataset_name, device)                   # load network
+strategy = get_strategy(config.strategy_name)(dataset, net)  # load strategy
 
 # start experiment
-dataset.initialize_labels(args.n_init_labeled)
-print(f"number of labeled pool: {args.n_init_labeled}")
-print(f"number of unlabeled pool: {dataset.n_pool-args.n_init_labeled}")
+dataset.initialize_labels(config.n_init_labeled)
+print(f"number of labeled pool: {config.n_init_labeled}")
+print(f"number of unlabeled pool: {dataset.n_pool-config.n_init_labeled}")
 print(f"number of testing pool: {dataset.n_test}")
 print()
 
@@ -51,13 +54,15 @@ print()
 print("Round 0")
 strategy.train()
 preds = strategy.predict(dataset.get_test_data())
-print(f"Round 0 testing accuracy: {dataset.cal_test_acc(preds)}")
+test_acc = dataset.cal_test_acc(preds)
+print(f"Round 0 testing accuracy: {test_acc}")
+wandb.log({"test/accuracy": test_acc, "round": 0})
 
-for rd in range(1, args.n_round+1):
+for rd in range(1, config.n_round+1):
     print(f"Round {rd}")
 
     # query
-    query_idxs = strategy.query(args.n_query)
+    query_idxs = strategy.query(config.n_query)
 
     # update labels
     strategy.update(query_idxs)
@@ -65,4 +70,6 @@ for rd in range(1, args.n_round+1):
 
     # calculate accuracy
     preds = strategy.predict(dataset.get_test_data())
-    print(f"Round {rd} testing accuracy: {dataset.cal_test_acc(preds)}")
+    test_acc = dataset.cal_test_acc(preds)
+    print(f"Round {rd} testing accuracy: {test_acc}")
+    wandb.log({"test/accuracy": test_acc, "round": rd})
---
+++ script.sh
@@ -0,0 +1,7 @@
+python demo.py \
+    --n_round 10 \
+    --n_query 1000 \
+    --n_init_labeled 10000 \
+    --dataset_name MNIST \
+    --strategy_name RandomSampling \
+    --seed 1
